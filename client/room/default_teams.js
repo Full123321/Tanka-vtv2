fa0b2f57999bffc3058b387462c06852a703557d
Full123321 <full123321e@gmail.com>
2026-09-26 01:11:18 +0700

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,35 +1,50 @@
 export function setupTeams() {
     try {
+        // Удаляем все существующие команды
         const allTeams = Teams.GetAll();
         for (let i = 0; i < allTeams.length; i++) {
             Teams.Remove(allTeams[i].Tag);
         }
 
-        Teams.Add('Black', 'Чёрные', { r: 0, g: 0, b: 0 });
+        // Создаем команду "Чёрные"
+        if (!Teams.Get('Black')) {
+            Teams.Add('Black', 'Чёрные', { r: 0, g: 0, b: 0 });
+        }
         const blackTeam = Teams.Get('Black');
-        const blueTeam = Teams.Get('Blue');
+        if (!blackTeam) return;
 
+        // Копируем спавны с "Синих" на "Чёрных"
+        const blueTeam = Teams.Get('Blue');
         if (blueTeam) {
             const blueSpawns = Spawns.GetContext(blueTeam);
             const blackSpawns = Spawns.GetContext(blackTeam);
 
-            for (let i = 0; i < blueSpawns.SpawnPointsGroups.Count; i++) {
-                const group = blueSpawns.SpawnPointsGroups.Get(i);
-                blackSpawns.SpawnPointsGroups.Add(group);
-            }
+            if (blueSpawns && blackSpawns) {
+                // Копируем группы спавнов
+                for (let i = 0; i < blueSpawns.SpawnPointsGroups.Count; i++) {
+                    const group = blueSpawns.SpawnPointsGroups.Get(i);
+                    blackSpawns.SpawnPointsGroups.Add(group);
+                }
 
-            for (let i = 0; i < blueSpawns.CustomSpawnPoints.Count; i++) {
-                const point = blueSpawns.CustomSpawnPoints.Get(i);
-                blackSpawns.CustomSpawnPoints.Add(point.X, point.Y, point.Z, point.Rotation);
+                // Копируем кастомные точки спавна
+                for (let i = 0; i < blueSpawns.CustomSpawnPoints.Count; i++) {
+                    const point = blueSpawns.CustomSpawnPoints.Get(i);
+                    blackSpawns.CustomSpawnPoints.Add(point.X, point.Y, point.Z, point.Rotation);
+                }
+                console.log("[Teams] Спавны успешно скопированы с Синих на Чёрных.");
             }
+        } else {
+            console.warn("[Teams] Команда 'Blue' не найдена на карте.");
         }
 
+        // Принудительное назначение команды
         Teams.OnPlayerChangeTeam.Add(function(player) {
-            if (player.Team.Tag !== 'Black') {
+            if (player && player.Team && player.Team.Tag !== 'Black') {
                 player.Team = blackTeam;
             }
         });
+
     } catch (e) {
-        console.error("Teams setup error: " + e.message);
+        console.error("Критическая ошибка настройки команд: " + e.message);
     }
 }
